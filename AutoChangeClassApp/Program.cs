a3690ce117ed3914bf0f25b2640e24b105007e74
Toan Tran <tranthetoan2003@gmail.com>
2024-04-16 15:05:54 +0700

    refactor app

---
+++ AutoChangeClassApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoChangeClassApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
